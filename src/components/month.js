f96daa5a14ec1f139984041c9d5ce3827d3ffd4c
Jhon <jhonb3r@gmail.com>
2019-11-02 13:14:46 -0500

    Add week day names

--- src/components/month.js
+++ src/components/month.js
@@ -10,21 +10,31 @@ export default class Month extends React.Component {
       name: 'January',
       year: 2019,
       day: 1,
-      board: Array(7).fill(Array(5))
+      board: Array(7).fill(Array(5).fill(0)),
+      daysOfWeek: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ]
     }
   }
 
-  createBoard (rows, columns) {
+  createBoard () {
+    let cnt = 1
     return (
-      this.state.board.map((val) => {
+      this.state.board.map((rows, dayIndex) => {
         return (
-          <div key={val} className='Column-style'>
+          <div key={rows + dayIndex} className='Column-style Header'>
             <div className='Month-header'>
-              <Header label='Monday' styleName='Day-name' />
+              <Header label={this.state.daysOfWeek[dayIndex]} styleName='Day-name' />
             </div>
-            {this.state.board.map((val2) => {
+            {rows.map((label, i) => {
               return (
-                <Day key={val2} label='2' />
+                <Day key={label + i} label={cnt++} />
               )
             })}
           </div>
@@ -34,6 +44,7 @@ export default class Month extends React.Component {
   }
 
   render () {
+    console.log(this.state.board)
     return (
       <div className='Row-style'>
         {this.createBoard()}
