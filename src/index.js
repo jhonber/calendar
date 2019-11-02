9c20342217e7f4af775d7dd99816b9ac986ab7de
Jhon <jhonb3r@gmail.com>
2019-11-02 18:44:53 -0500

    Integrate redux with react and add initial tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
+import configureStore from './redux/store/configureStore'
+import { addReminder } from './redux/actions/reminders'
+import getSortedReminders from './redux/selectors/reminders'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = configureStore()
+console.log(store.getState())
+
+store.dispatch(addReminder({
+  text: 'Cumple',
+  color: 'blue',
+  date: '20 nov',
+  time: '12:00',
+  city: 'London'
+}))
+
+store.dispatch(addReminder({
+  text: 'Cita',
+  color: 'amarillo',
+  date: '4 dic',
+  time: '19:00',
+  city: 'Pereira'
+}))
+
+const state = store.getState()
+console.log(state)
+const data = getSortedReminders(state.reminders, state.filters)
+console.log(data)
+
+const jsx = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
