bb8ef304b16cf42d2d5e67e6590e58901b0b36ab
Jhon <jhonb3r@gmail.com>
2019-11-03 12:12:03 -0500

    Manage change of month forward and backward, recalculation the correct info

--- src/redux/reducers/months.js
+++ src/redux/reducers/months.js
@@ -1,14 +1,45 @@
 
 import moment from 'moment'
 
-const currentMonth = moment().month()
+let nextMonth, nextYear, nextDate, nextInitialDayOfWeek, nextNumberOfDays
+const currentDate = moment()
+const monthInit = {
+  year: currentDate.year(),
+  month: currentDate.month(),
+  initialDayOfWeek: currentDate.weekday(),
+  numberOfDays: currentDate.daysInMonth()
+}
 
-const months = (state = currentMonth, action) => {
+const getDate = (year, month) => {
+  return moment(year + '-' + (month + 1) + '-01', 'YYYY-MM-DD')
+}
+
+const months = (state = monthInit, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return (state + 1) % 12
+      nextMonth = (state.month + 1) % 12
+      nextYear = nextMonth === 0 ? state.year + 1 : state.year
+      nextDate = getDate(nextYear, nextMonth)
+      nextInitialDayOfWeek = nextDate.weekday()
+      nextNumberOfDays = nextDate.daysInMonth()
+      return {
+        year: nextYear,
+        month: nextMonth,
+        initialDayOfWeek: nextInitialDayOfWeek,
+        numberOfDays: nextNumberOfDays
+      }
     case 'DECREMET':
-      return (state - 1 + 12) % 12
+      nextMonth = (state.month - 1 + 12) % 12
+      nextYear = nextMonth === 11 ? state.year - 1 : state.year
+      nextDate = getDate(nextYear, nextMonth)
+      nextInitialDayOfWeek = nextDate.weekday()
+      nextNumberOfDays = nextDate.daysInMonth()
+      return {
+        year: nextYear,
+        month: nextMonth,
+        initialDayOfWeek: nextInitialDayOfWeek,
+        numberOfDays: nextNumberOfDays
+      }
     default:
       return state
   }
