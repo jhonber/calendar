7685312a8ed39549b584282b0de14c669520eec6
Jhon <jhonb3r@gmail.com>
2019-11-03 21:54:11 -0500

    Add simple view for reminders

--- src/components/day.js
+++ src/components/day.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import Modal from './modal'
 import '../App.css'
 
 const MAX_ITEMS_TO_LIST = 2
 
 export default class Day extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showModal: false,
+      body: null
+    }
+    this.handleClickOnItem = this.handleClickOnItem.bind(this)
+  }
+
+  handleToggleModal () {
+    this.setState({
+      showModal: !this.state.showModal
+    })
+  }
+
+  handleClickOnItem (item) {
+    const body = <div>
+      <span>{item.color}</span>
+      <span>{item.text}</span>
+      <span>{item.city}</span>
+      <span>{item.date.format('YYYY-MM-DD')}</span>
+      <span>{item.time}</span>
+    </div>
+
+    this.setState({
+      showModal: true,
+      body: body
+    })
+  }
+
   render () {
     const itemsToList = this.props.items.slice(0, MAX_ITEMS_TO_LIST)
     const rest = this.props.items.length - itemsToList.length
@@ -20,11 +51,23 @@ export default class Day extends React.Component {
         <div className='Items'>
           <ul>
             {itemsToList.map((item, cnt) => {
-              return <li key={item.id}> {item.text} </li>
+              return (
+                <li
+                  key={item.id}
+                  onClick={() => this.handleClickOnItem(item)}
+                >
+                  {item.text}
+                </li>
+              )
             })}
             {overflow}
           </ul>
         </div>
+        <Modal
+          visible={this.state.showModal}
+          body={this.state.body}
+          handleToggleModal={() => this.handleToggleModal()}
+        />
       </div>
     )
   }
