61c34dc349e6561fd45e3431a78872d521ba3315
Jhon <jhonb3r@gmail.com>
2019-11-04 22:41:58 -0500

    Update style

--- src/components/createReminderForm.js
+++ src/components/createReminderForm.js
@@ -72,59 +72,71 @@ export default class CreateReminderForm extends React.Component {
 
   render () {
     return (
-      <Form
-        noValidate
-        validated={this.state.validated}
-        onSubmit={this.handleSubmit}
-        id='createFormId'
-      >
-        <Form.Group controlId='formBasicEmail' className='createForm'>
-          <HuePicker
-            width='100%'
-            color={this.state.color}
-            onChangeComplete={this.handleColor}
-          />
-          <Form.Control
-            onChange={(e) => this.handleText(e)}
-            type='text'
-            placeholder='Text'
-            maxLength={30}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>
-              Please enter a text (30 max)
-          </Form.Control.Feedback>
-          <Form.Control
-            onChange={(e) => this.handleCity(e)}
-            type='text'
-            placeholder='City'
-            required
-          />
-          <Form.Control.Feedback type='invalid'>
-              Please enter a city
-          </Form.Control.Feedback>
-          <DatePicker
-            selected={this.state.date}
-            onChange={date => this.handleDate(date)}
-          />
-          <DatePicker
-            selected={this.state.time}
-            onChange={time => this.handleTime(time)}
-            showTimeSelect
-            showTimeSelectOnly
-            timeFormat='HH:mm'
-            timeIntervals={60}
-            timeCaption='time'
-            dateFormat='HH:mm'
-          />
-        </Form.Group>
-        <Button
-          type='submit'
-          className='actionButton'
+      <div className='createForm'>
+        <Form
+          noValidate
+          validated={this.state.validated}
+          onSubmit={this.handleSubmit}
+          id='createFormId'
         >
-          Create
-        </Button>
-      </Form>
+          <Form.Group controlId='formBasicEmail' className='createForm'>
+            <div className='inputField'>
+              <HuePicker
+                width='100%'
+                color={this.state.color}
+                onChangeComplete={this.handleColor}
+              />
+            </div>
+            <div className='inputField'>
+              <Form.Control
+                onChange={(e) => this.handleText(e)}
+                type='text'
+                placeholder='Text'
+                maxLength={30}
+                required
+              />
+              <Form.Control.Feedback type='invalid'>
+                Please enter a text (30 max)
+              </Form.Control.Feedback>
+            </div>
+            <div className='inputField'>
+              <Form.Control
+                onChange={(e) => this.handleCity(e)}
+                type='text'
+                placeholder='City'
+                required
+              />
+              <Form.Control.Feedback type='invalid'>
+                  Please enter a city
+              </Form.Control.Feedback>
+            </div>
+            <div className='inputField'>
+              <DatePicker
+                selected={this.state.date}
+                onChange={date => this.handleDate(date)}
+              />
+            </div>
+            <div className='inputField'>
+              <DatePicker
+                selected={this.state.time}
+                onChange={time => this.handleTime(time)}
+                showTimeSelect
+                showTimeSelectOnly
+                timeFormat='HH:mm'
+                timeIntervals={60}
+                timeCaption='time'
+                dateFormat='HH:mm'
+              />
+            </div>
+          </Form.Group>
+          <Button
+            type='submit'
+            className='actionButton'
+          >
+            Create
+          </Button>
+        </Form>
+      </div>
     )
   }
 }
