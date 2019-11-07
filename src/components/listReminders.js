c7963506d857fbbe1f24d7eae2e247b554c9e5ae
Jhon <jhonb3r@gmail.com>
2019-11-07 18:13:45 -0500

    Add component to list reminders

---
+++ src/components/listReminders.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import '../App.css'
+
+class ListReminder extends React.Component {
+  renderReminders (reminders) {
+    return reminders.map((item, cnt) => {
+      return (
+        <div key={item.id + cnt} className='Row-style'>
+          <li
+            style={{ backgroundColor: item.color }}
+            key={item.id}
+            onClick={() => this.props.handleClick(item)}
+          >
+            {item.text}
+          </li>
+        </div>
+      )
+    })
+  }
+
+  render () {
+    console.log('props**')
+    console.log(this.props)
+    const remindersToList = this.props.showAll
+      ? this.props.reminders
+      : this.props.reminders.slice(0, this.props.total)
+
+    const rest = !this.props.showAll
+      ? this.props.reminders.length - remindersToList.length
+      : 0
+    const overflow = rest > 0
+      ? <p
+        onClick={() => this.props.handleClickShowMore}>
+        {rest} more
+      </p>
+      : null
+
+    return (
+      <div className='Items'>
+        <ul>
+          {this.renderReminders(remindersToList)}
+          {overflow}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default ListReminder
